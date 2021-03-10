e1152ae0bfc45a945320ca4dbc69a21b7054650f
Gerald Pape <ubergesundheit@users.noreply.github.com>
2021-03-10 12:00:46 +0100

    Replace Travis with Github Actions (#35)
    * add test workflow stub
    
    * matrix check all versions
    
    * try using giantswarm/install-binary-action
    
    * use full commit hash for install-binary-action
    
    * set tarball_binary_path in install_binary_action
    
    * add dependabot configuration
    
    * update sketch-templater
    
    * execute tests in ci
    
    * remove travis config
    
    * cleanup actions files
    
    * remove stray comment from workflow
    
    * fix package_sensebox_index.json
    
    Remove versions not available from releases
    
    * use sensors from models
    
    * validate stable packages only on master push
    
    * validate dev package
    
    * remove travis badges from readme
    
    * use sudo for dev validation preparation
    
    * show nginx status
    
    * merge setup & validate step
    
    * compile with dev package
    
    * try to explain dev + release
    
    * add/update libraries
    
    * fix Adafruit_BusIO location
    
    * Only cache arduino not sensebox packages
    
    Co-authored-by: Mario <mario.pesch@uni-muenster.de>

--- arduino-test/build-sketches.js
+++ arduino-test/build-sketches.js
@@ -1,94 +1,62 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const child_process = require('child_process');
+const { mkdirSync, writeFileSync } = require("fs");
+const { execSync } = require("child_process");
 
-const SketchTemplater = require('@sensebox/sketch-templater');
+const SketchTemplater = require("@sensebox/sketch-templater");
 
-const sketchTemplater = new SketchTemplater('test.ingress.domain');
+const { getSensorsForModel } = require("./sensorLayouts");
+
+const sketchTemplater = new SketchTemplater("test.ingress.domain");
+
+const genId = (i) => `59479ed5a4ad5900112d8d${i.toString(16).padStart(2, "0")}`;
 
 const boxStub = function (model) {
   return {
-    _id: '59479ed5a4ad5900112d8dec',
+    _id: "59479ed5a4ad5900112d9d00",
     model: model,
-    sensors: [
-      {
-        title: 'Temperatur',
-        _id: '59479ed5a4ad5900112d8ded',
-        sensorType: 'HDC1080'
-      },
-      {
-        title: 'rel. Luftfeuchte',
-        _id: '59479ed5a4ad5900112d8dee',
-        sensorType: 'HDC1080'
-      },
-      {
-        title: 'Luftdruck',
-        _id: '59479ed5a4ad5900112d8def',
-        sensorType: 'BMP280'
-      },
-      {
-        title: 'Beleuchtungsstärke',
-        _id: '59479ed5a4ad5900112d8df0',
-        sensorType: 'TSL45315'
-      },
-      {
-        title: 'UV-Intensität',
-        _id: '59479ed5a4ad5900112d8df1',
-        sensorType: 'VEML6070'
-      },
-      {
-        title: 'PM10',
-        _id: '59479ed5a4ad5900112d8df2',
-        sensorType: 'SDS011'
-      },
-      {
-        title: 'PM2.5',
-        _id: '59479ed5a4ad5900112d8df3',
-        sensorType: 'SDS011'
-      }
-    ],
-    serialPort: 'Serial1',
-    ssid: 'MY-HOME-NETWORK',
-    password: 'MY-SUPER-PASSWORD'
+    sensors: getSensorsForModel(model).map(({ title, sensorType }, index) => ({
+      title,
+      sensorType,
+      _id: genId(index),
+    })),
+    serialPort: "Serial1",
+    ssid: "MY-HOME-NETWORK",
+    password: "MY-SUPER-PASSWORD",
   };
 };
 
 const buildMatrix = {
-  'sensebox:samd:sb': []
+  "sensebox:samd:sb": [],
 };
 
 for (const model of Object.keys(sketchTemplater._templates)) {
-  if (model.includes('V2')) {
-    buildMatrix['sensebox:samd:sb'].push(model);
+  if (model.includes("V2")) {
+    buildMatrix["sensebox:samd:sb"].push(model);
   }
 }
 
 const mkdirp = function mkdirp(path) {
   /* eslint-disable no-empty */
   try {
-    fs.mkdirSync(path);
-  } catch (e) { }
+    mkdirSync(path);
+  } catch (e) {}
   /* eslint-enable no-empty */
 };
 
 const build = function build(board, model) {
   mkdirp(`${sketchesPath}/${model}`);
-  fs.writeFileSync(
+  writeFileSync(
     `${sketchesPath}/${model}/${model}.ino`,
     sketchTemplater.generateSketch(boxStub(model))
   );
-  console.log(
-    `Building model ${model} with "arduino --verbose-build --verify --board ${board} ${sketchesPath}/${model}/${model}.ino"`
-  );
-  child_process.execSync(
-    `arduino --verbose-build --verify --board ${board} ${sketchesPath}/${model}/${model}.ino`,
-    { stdio: [0, 1, 2] }
-  );
-  console.log('###########################################################');
+  const cmdWithArgs = `arduino-cli compile --fqbn ${board} ${sketchesPath}/${model}/${model}.ino`;
+  console.log(`Building model ${model} with "${cmdWithArgs}"`);
+  execSync(cmdWithArgs, { stdio: [0, 1, 2] });
+  console.log("###########################################################");
 };
 
-const sketchesPath = `${__dirname}/sketches`;
+const sketchesPath = `/tmp/sketches`;
 mkdirp(sketchesPath);
 
 for (const board of Object.keys(buildMatrix)) {
@@ -96,4 +64,4 @@ for (const board of Object.keys(buildMatrix)) {
     build(board, model);
   }
 }
-console.log('done');
+console.log("done");
